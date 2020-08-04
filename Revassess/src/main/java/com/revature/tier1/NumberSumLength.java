e9b9b084392ee3b7800cfc37b2ec03846f67678d
Patrick Boronski <plboronski@gmail.com>
2020-08-04 15:02:20 -0400

    adding implemented code

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+    	String sNum = Long.toString(num);
+    	long sum = 0;
+    	int digit;
+    	
+    	for(int i=1; i<=sNum.length(); i++) {
+    		digit = Integer.parseInt(sNum.substring(i-1, i));
+    		sum += Math.pow(digit, sNum.length());
+    	}
+    	
+    	return sum == num;
 	}
 }
